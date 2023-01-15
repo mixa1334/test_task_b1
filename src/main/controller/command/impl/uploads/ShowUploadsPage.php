a88b9b727e8874dce10217bd41003271a254fa7c
mixa1334 <micha252282@gmail.com>
2023-01-15 17:05:02 +0300

    fix

--- src/main/controller/command/impl/uploads/ShowUploadsPage.php
+++ src/main/controller/command/impl/uploads/ShowUploadsPage.php
@@ -1,11 +1,13 @@
 <?php
 require_once __DIR__ . "/../../Command.php";
 require_once __DIR__ . "/../../../Router.php";
+
 class ShowUploadsPage implements Command
 {
     public function execute(): Router
     {
         // TODO: Implement execute() method.
+        return new Router(null);
     }
 
 }
--- src/main/controller/command/impl/uploads/file/DeleteFile.php
+++ src/main/controller/command/impl/uploads/file/DeleteFile.php
@@ -1,11 +1,13 @@
 <?php
-require_once __DIR__ . "/../../Command.php";
-require_once __DIR__ . "/../../../Router.php";
+require_once __DIR__ . "/../../../Command.php";
+require_once __DIR__ . "/../../../../Router.php";
+
 class DeleteFile implements Command
 {
     public function execute(): Router
     {
         // TODO: Implement execute() method.
+        return new Router(null);
     }
 
 }
